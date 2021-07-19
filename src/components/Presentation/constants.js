9aaa532196be06b33060b54d4f8fde43b5d3f284
FlorianPx <florian.poux@hotmail.fr>
2021-07-19 17:53:13 +0200

    Create themes document and components presentation

---
+++ src/components/Presentation/constants.js
@@ -0,0 +1,8 @@
+import { title } from "../Header/constants";
+
+export const presentationTextOne =
+  "Diplômé en pâtisserie, je cuisine depuis maintenant trois ans dans mon laboratoire tous types de viennoiseries et gâteaux.";
+
+const galettes = "galettes au sarrasin";
+const cvp = "café avec viennoiseries ou pâtisseries";
+export const presentationTextTwo = `Désirant partager ma passion, j’ai eu l’idée de créer ${title.bold()}, un concept de cuisine nomade où je vous propose le soir un menu composé de ${galettes.bold()} et le matin un ${cvp.bold()}. Le tout fait maison.`;
---
+++ src/themes/themes.js
@@ -0,0 +1,8 @@
+export const themes = {
+  width: "1em",
+  title: "#c1254a",
+  pink: "#de5475",
+  yellow: "#cf8c17",
+};
+
+export default themes;
