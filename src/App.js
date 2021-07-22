97ea4445cd0441156754b03831ae25c717b5e2f0
FlorianPx <florian.poux@hotmail.fr>
2021-07-22 13:13:51 +0200

    Integration of presentation on App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css";
+import Presentation from "./components/Presentation/Presentation";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Presentation />
     </div>
   );
 }
