c34a2cb8372244553bf8b97f202343c5a700c718
Florian <68642839+FlorianPx@users.noreply.github.com>
2023-01-13 19:33:26 +0100

    Update constants.js

--- src/components/Footer/SocialNetwork/constants.js
+++ src/components/Footer/SocialNetwork/constants.js
@@ -2,14 +2,5 @@ import FacebookLogo from "../../../assets/svg/facebook.svg";
 import MessengerLogo from "../../../assets/svg/messenger.svg";
 
 export const socialNetWorkArray = [
-  {
-    title: "Facebook",
-    src: FacebookLogo,
-    url: "https://www.facebook.com/thierrylapatisseire.tampie.5",
-  },
-  {
-    title: "Messenger",
-    src: MessengerLogo,
-    url: "https://www.messenger.com/t/100029843478719",
-  },
+ 
 ];
