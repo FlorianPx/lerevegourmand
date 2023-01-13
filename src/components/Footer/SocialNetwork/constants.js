683bb08f0157c3510ff305b95d925bc21cf5cf9f
Florian <68642839+FlorianPx@users.noreply.github.com>
2023-01-13 19:33:44 +0100

    Update constants.js

--- src/components/Footer/SocialNetwork/constants.js
+++ src/components/Footer/SocialNetwork/constants.js
@@ -1,6 +1,4 @@
 import FacebookLogo from "../../../assets/svg/facebook.svg";
 import MessengerLogo from "../../../assets/svg/messenger.svg";
 
-export const socialNetWorkArray = [
- 
-];
+export const socialNetWorkArray = [];
