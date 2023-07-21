37d90f9ba1da664a80357dc07f7cf1ccabae73f1
Florian <florian.poux@hotmail.fr>
2023-07-21 15:28:04 +0200

    feat: modal integration in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import Header from "./components/Header";
+import { WebsiteClosingModal } from "./components/WebsiteClosingModal/WebsiteClosingModal";
 import Informations from "./components/Informations/Informations";
-import Menu from "./components/Menu/Menu";
 import Pastries from "./components/Pastries/Pastries";
+import { Modal } from "./components/Modal/Modal";
 import Footer from "./components/Footer/Footer";
+import Menu from "./components/Menu/Menu";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
+  const [modalContent, setModalContent] = useState({
+    body: null,
+    title: null,
+  });
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = (content) => {
+    setIsOpen(true);
+    setModalContent(content);
+  };
+
+  useEffect(() => {
+    openModal({
+      title: "",
+      body: <WebsiteClosingModal />,
+    });
+  }, []);
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
+      <Modal closeModal={closeModal} content={modalContent} isOpen={isOpen} />
       <div className="App">
         <Header />
         <Informations />
